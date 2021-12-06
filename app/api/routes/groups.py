0c0b3d8f5404dcf075ffeadd985fcaa83bd65519
Ruslan Salkaev <rsalkaev@gmail.com>
2021-12-06 20:45:42 +0300

    More refactoring

--- app/api/routes/groups.py
+++ app/api/routes/groups.py
@@ -17,13 +17,13 @@ async def my_groups(current_user: User = Depends(get_current_user), db: Session 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_group(request: GroupCreationRequest, current_user: User = Depends(get_current_user),
-                           db: Session = Depends(get_database)):
+                       db: Session = Depends(get_database)):
     return groups_service.create_group(db, current_user, request)
 
 
 @router.get("/members")
 async def get_group_members(group_id: int, current_user: User = Depends(get_current_user),
-                        db: Session = Depends(get_database)):
+                            db: Session = Depends(get_database)):
     return groups_service.get_group_members(db, current_user, group_id)
 
 
